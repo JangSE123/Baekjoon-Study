3a60dc4b23adeb300608cf9490f8352fd9691b89
JangSE123 <jse010206@gmail.com>
2025-05-07 20:15:20 +0900

    solve: Baekjoon Stage 2
    🎯 Stage: 1 - Conditional statement
    📝 Problems Solved: 1330, 9498
    🛠️ Languages Used: Java / Python / C

---
+++ Stage2/Java/Problem1330.java
@@ -0,0 +1,22 @@
+package Stage2.Java;
+
+import java.util.Scanner;
+
+public class Problem1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+
+        sc.close();
+    }
+}
