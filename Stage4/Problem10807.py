5eebfbec9a08b5e8fb3d94252432c6a2914f1c1f
JangSE123 <jse010206@gmail.com>
2025-05-19 19:46:54 +0900

    solve: Baekjoon Stage 4
    🎯 Stage: 4 - One-dimensional array
    📝 Problems Solved: 10807, 10871
    🛠️ Languages Used: Java / Python / C

---
+++ Stage4/Problem10807.py
@@ -0,0 +1,8 @@
+def main():
+    N = int(input())
+    arr = list(map(int, input().split()))
+    v = int(input())   
+    print(arr.count(v))
+    
+if __name__ == "__main__":
+    main()
---
+++ Stage4/Problem10871.py
@@ -0,0 +1,7 @@
+def main():
+    N, X = map(int, input().split())
+    A = list(map(int, input().split()))
+    print(*[num for num in A if num < X])
+    
+if __name__ == "__main__":
+    main()
