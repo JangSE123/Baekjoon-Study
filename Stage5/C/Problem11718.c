1775a6e4a3aae7244566a4900be6ca29717282d6
JangSE123 <jse010206@gmail.com>
2025-06-05 17:06:37 +0900

    solve: Baekjoon Stage 5
    🎯 Stage: 5 - String
    📝 Problems Solved: 11718
    🛠️ Languages Used: Java / Python / C

---
+++ Stage5/C/Problem11718.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char line[101];
+
+    while (fgets(line, sizeof(line), stdin) != NULL) {
+        printf("%s", line);
+    }
+
+    return 0;
+}
---
+++ Stage5/Java/Problem11718.java
@@ -0,0 +1,21 @@
+package Stage5.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
