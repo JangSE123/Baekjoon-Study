90218efd6392c5aba397d8b43b592196edf4b348
JangSE123 <jse010206@gmail.com>
2025-06-16 14:46:22 +0900

    solve: Baekjoon Stage 7
    🎯 Stage: 7 - Two-dimensional array
    📝 Problems Solved: 10798
    🛠️ Languages Used: Java / Python / C

---
+++ Stage7/C/Problem10798.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    char arr[5][16] = {0,};
+
+    for (int i = 0; i < 5; i++) {
+        scanf("%s", arr[i]);
+    }
+
+    for (int j = 0; j < 15; j++) {
+        for (int i = 0; i < 5; i++) {
+            if (arr[i][j] != '\0') {
+                printf("%c", arr[i][j]);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ Stage7/Python/Problem10798.py
@@ -0,0 +1,10 @@
+def main():
+    arr = [input() for _ in range(5)]
+    
+    for i in range(15):
+        for j in range(5):
+            if i < len(arr[j]):
+                print(arr[j][i], end = '')
+    
+if __name__ == "__main__":
+    main()
