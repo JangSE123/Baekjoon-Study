be1814cd7c34476af90c8232ea8eaff8de984e96
JangSE123 <jse010206@gmail.com>
2025-09-19 17:34:25 +0900

    solve: Baekjoon Stage8
    🎯 Stage: 8 - Mathematics 1
    📝 Problems Solved: 2292
    🛠️ Languages Used: Java

---
+++ Stage8/Java/Problem2292.java
@@ -0,0 +1,29 @@
+package Stage8.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2292 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        int layer = 1;
+        int maxNum = 1;
+
+        while (maxNum < N) {
+            maxNum += 6 * layer;
+            layer++;
+        }
+
+        System.out.println(layer);
+        br.close();
+    }
+}
