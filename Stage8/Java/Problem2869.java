ccaf109bcf46fc1ea9cd87c8715f84cd60ab508a
JangSE123 <jse010206@gmail.com>
2025-09-20 19:18:43 +0900

    solve: Baekjoon Stage8
    🎯 Stage: 8 - Mathematics 1
    📝 Problems Solved: 2869
    🛠️ Languages Used: Java

---
+++ Stage8/Java/Problem2869.java
@@ -0,0 +1,26 @@
+package Stage8.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int days = (V - A) / (A - B);
+        if ((V - A) % (A - B) != 0) {
+            days++;
+        }
+        days++;
+            
+        System.out.println(days);
+        br.close();
+    }
+}
