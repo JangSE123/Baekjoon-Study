9997fa734f076f15057ee652d693211133c8bdb3
JangSE123 <jse010206@gmail.com>
2025-09-22 19:08:47 +0900

    solve: Baekjoon Stage9
    🎯 Stage: 9 - Divisors, Multiples, and Primes 1
    📝 Problems Solved: 2501
    🛠️ Languages Used: C

---
+++ Stage9/C/Problem2501.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int N, K, count = 0, result = 0;
+    scanf("%d %d", &N, &K);
+
+    for (int i = 1; i <= N; i++) {
+        if (N % i == 0) {
+            count++;
+            if (count == K) {
+                result = i;
+                break;
+            }
+        }
+    }
+    
+    printf("%d", result);
+    return 0;
+}
