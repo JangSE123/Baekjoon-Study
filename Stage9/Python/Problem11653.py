906fcffecea5c1756368707c7c1a84dfc19b13cf
JangSE123 <jse010206@gmail.com>
2025-09-24 20:12:03 +0900

    solve: Baekjoon Stage9
    🎯 Stage: 9 - Divisors, Multiples, and Primes 1
    📝 Problems Solved: 11653
    🛠️ Languages Used: Python

---
+++ Stage9/Python/Problem11653.py
@@ -0,0 +1,18 @@
+def main():
+    N = int(input())
+    
+    if N == 1:
+        exit()
+        
+    i = 2
+    while i * i <= N:
+        while N % i == 0:
+            print(i)
+            N //= i
+        i += 1
+    
+    if N > 1:
+        print(N)
+    
+if __name__ == "__main__":
+    main()
