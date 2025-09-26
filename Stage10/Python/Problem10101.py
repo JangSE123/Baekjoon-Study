86edb38260a27a46d4c1e59437f914cedda0a861
JangSE123 <jse010206@gmail.com>
2025-09-26 18:59:44 +0900

    solve: Baekjoon Stage10
    🎯 Stage: 10 - Geometry 1: Rectangles and Triangles
    📝 Problems Solved: 10101
    🛠️ Languages Used: Python

---
+++ Stage10/Python/Problem10101.py
@@ -0,0 +1,16 @@
+def main():
+    angle1 = int(input())
+    angle2 = int(input())
+    angle3 = int(input())
+    
+    if angle1 + angle2 + angle3 != 180:
+        print("Error")
+    elif angle1 == angle2 and angle2 == angle3:
+        print("Equilateral")
+    elif angle1 == angle2 or angle2 == angle3 or angle1 == angle3:
+        print("Isosceles")
+    else:
+        print("Scalene")
+    
+if __name__ == "__main__":
+    main()
