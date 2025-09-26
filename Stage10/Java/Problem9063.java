df004ce5c2a3fa49d8f70e9d43799ab90784d05c
JangSE123 <jse010206@gmail.com>
2025-09-26 18:39:53 +0900

    solve: Baekjoon Stage10
    🎯 Stage: 10 - Geometry 1: Rectangles and Triangles
    📝 Problems Solved: 9063
    🛠️ Languages Used: Java

---
+++ Stage10/Java/Problem9063.java
@@ -0,0 +1,35 @@
+package Stage10.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem9063 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
+        }
+
+        int width = maxX - minX;
+        int height = maxY - minY;
+
+        System.out.println(width * height);
+        br.close();
+    }
+}
