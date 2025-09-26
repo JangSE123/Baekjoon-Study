f894ec7b03d4305b426b2bb077e96795b16b0f2d
JangSE123 <jse010206@gmail.com>
2025-09-26 17:38:05 +0900

    solve: Baekjoon Stage10
    🎯 Stage: 10 - Geometry 1: Rectangles and Triangles
    📝 Problems Solved: 15894
    🛠️ Languages Used: Java / Python / C

---
+++ Stage10/Java/Problem15894.java
@@ -0,0 +1,14 @@
+package Stage10.Java;
+
+import java.util.Scanner;
+
+public class Problem15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(n * 4);
+        sc.close();
+    }
+}
