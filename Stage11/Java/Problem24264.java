6e544aa4fcf4b3b9d83adf02dc5eb55d53f5ec76
JangSE123 <jse010206@gmail.com>
2025-09-29 17:41:34 +0900

    solve: Baekjoon Stage11
    🎯 Stage: 11 - Time Complexity
    📝 Problems Solved: 24264
    🛠️ Languages Used: Java / Python / C

---
+++ Stage11/Java/Problem24264.java
@@ -0,0 +1,14 @@
+package Stage11.Java;
+
+import java.util.Scanner;
+
+public class Problem24264 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        System.out.println(n * n);
+        System.out.println(2);
+        sc.close();
+    }
+}
