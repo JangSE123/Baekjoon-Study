8c18fb3f9cdc584b9274be1334708c7268b0740f
JangSE123 <jse010206@gmail.com>
2025-09-29 16:40:25 +0900

    solve: Baekjoon Stage11
    🎯 Stage: 11 - Time Complexity
    📝 Problems Solved: 24262
    🛠️ Languages Used: Java / Python / C

---
+++ Stage11/Java/Problem24262.java
@@ -0,0 +1,14 @@
+package Stage11.Java;
+
+import java.util.Scanner;
+
+public class Problem24262 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.println(1);
+        System.out.println(0);
+        sc.close();
+    }
+}
---
+++ Stage11/Python/Problem24262.py
@@ -0,0 +1,6 @@
+def main():
+    n = int(input())
+    print("1\n0")
+    
+if __name__ == "__main__":
+    main()
