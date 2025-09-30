01f1fa6d85927d317cea6746239ce304b1baec11
JangSE123 <jse010206@gmail.com>
2025-09-30 20:14:54 +0900

    solve: Baekjoon Stage12
    🎯 Stage: 12 - Brute Force
    📝 Problems Solved: 2798
    🛠️ Languages Used: Python

---
+++ Stage12/Python/Problem2798.py
@@ -0,0 +1,17 @@
+def main():
+    N, M = map(int, input().split())
+    cards = list(map(int, input().split()))
+    
+    result = 0
+    
+    for i in range(N - 2):
+        for j in range(i + 1, N - 1):
+            for k in range(j + 1, N):
+                s = cards[i] + cards[j] + cards[k]
+                if s <= M and s > result:
+                    result = s
+    
+    print(result)
+    
+if __name__ == "__main__":
+    main()
