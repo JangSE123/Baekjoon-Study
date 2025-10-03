b7fa3751580bc13f37bf53d6dd8eae68483b846f
JangSE123 <jse010206@gmail.com>
2025-10-03 23:20:55 +0900

    solve: Baekjoon Stage12
    🎯 Stage: 12 - Brute Force
    📝 Problems Solved: 19532
    🛠️ Languages Used: Python

---
+++ Stage12/Python/Problem19532.py
@@ -0,0 +1,11 @@
+def main():
+    a, b, c, d, e, f = map(int, input().split())
+    
+    denominator = a * e - b * d
+    x = (c * e - b * f) // denominator
+    y = (a * f - c * d) // denominator
+
+    print(x, y)
+    
+if __name__ == "__main__":
+    main()
