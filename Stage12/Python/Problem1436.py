f80c10a15f7b8c1f901cbdab3934eb6ab8c650ef
JangSE123 <jse010206@gmail.com>
2025-10-06 21:21:19 +0900

    solve: Baekjoon Stage12
    🎯 Stage: 12 - Brute Force
    📝 Problems Solved: 1436
    🛠️ Languages Used: Python

---
+++ Stage12/Python/Problem1436.py
@@ -0,0 +1,15 @@
+def main():
+    N = int(input())
+    num = 666
+    count = 0
+    
+    while True:
+        if "666" in str(num):
+            count += 1
+        if count == N:
+            print(num)
+            break
+        num += 1
+    
+if __name__ == "__main__":
+    main()
