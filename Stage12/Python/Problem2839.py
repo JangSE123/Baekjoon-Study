0a080003b0e5e21ae5dd521e1181ff9e758d30a6
JangSE123 <jse010206@gmail.com>
2025-10-06 21:56:46 +0900

    solve: Baekjoon Stage12
    🎯 Stage: 12 - Brute Force
    📝 Problems Solved: 2839
    🛠️ Languages Used: Python

---
+++ Stage12/Python/Problem2839.py
@@ -0,0 +1,14 @@
+def main():
+    N = int(input())
+    result = -1
+    
+    for i in range(N // 5, -1, -1):
+        remain = N - (i * 5)
+        if remain % 3 == 0:
+            result = i + (remain // 3)
+            break
+    
+    print(result)
+    
+if __name__ == "__main__":
+    main()
