ca53e4ca139097482eaeecb98682f195e40c959d
JangSE123 <jse010206@gmail.com>
2025-10-09 14:24:05 +0900

    solve: Baekjoon Stage13
    🎯 Stage: 13 - Sorting Algorithm
    📝 Problems Solved: 10989
    🛠️ Languages Used: Java

---
+++ Stage13/Java/Problem10989.java
@@ -0,0 +1,29 @@
+package Stage13.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[num]++;
+        }
+
+        for (int i = 1; i <= 10000; i++) {
+            while (arr[i]-- > 0) {
+                sb.append(i).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
