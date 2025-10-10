8aa77e748b28de0400b534111ad8bcd3fb3cfb20
JangSE123 <jse010206@gmail.com>
2025-10-10 19:44:43 +0900

    solve: Baekjoon Stage13
    🎯 Stage: 13 - Sorting Algorithm
    📝 Problems Solved: 11650
    🛠️ Languages Used: Java

---
+++ Stage13/Java/Problem11650.java
@@ -0,0 +1,38 @@
+package Stage13.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        int[][] points = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]);
+            } else {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            sb.append(points[i][0]).append(" ").append(points[i][1]).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
