ab69e29dfdc485c738f6edcb5a92239fa51dbb4b
JangSE123 <jse010206@gmail.com>
2025-11-17 17:46:35 +0900

    solve: Baekjoon Stage16
    🎯 Stage: 16 - Stack, Queue, and Deque 1
    📝 Problems Solved: 2164
    🛠️ Languages Used: Java

---
+++ Stage16/Java/Problem2164.java
@@ -0,0 +1,26 @@
+package Stage16.Java;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Problem2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            q.add(q.poll());
+        }
+
+        System.out.println(q.poll());
+        sc.close();
+    }
+}
