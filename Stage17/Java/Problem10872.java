5b4ca8ab4ab9b9fe045ebd3496af0e67b1f1b582
JangSE123 <jse010206@gmail.com>
2025-11-21 10:22:09 +0900

    solve: Baekjoon Stage17
    🎯 Stage: 17 - Combinatorics
    📝 Problems Solved: 24723
    🛠️ Languages Used: Java / Python / C

---
+++ Stage17/Java/Problem10872.java
@@ -0,0 +1,19 @@
+package Stage17.Java;
+
+import java.util.Scanner;
+
+public class Problem10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= N; i++) {
+            result *= i;
+        }
+
+        System.out.println(result);
+        sc.close();
+    }
+}
---
+++ Stage17/Python/Problem10872.py
@@ -0,0 +1,10 @@
+import math
+
+def main():
+    N = int(input())
+    result = math.factorial(N)
+    
+    print(result)
+    
+if __name__ == "__main__":
+    main()
