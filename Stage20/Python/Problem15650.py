0ae863377b866304f5bc26e05eed58dd75a9d6ab
JangSE123 <jse010206@gmail.com>
2025-12-04 17:09:06 +0900

    solve: Baekjoon Stage20
    🎯 Stage: 20 - Backtracking
    📝 Problems Solved: 15650
    🛠️ Languages Used: Python

---
+++ Stage20/Python/Problem15650.py
@@ -0,0 +1,18 @@
+def dfs(depth, start):
+    if depth == M:
+        print(*arr)
+        return
+
+    for i in range(start, N + 1):
+        arr.append(i)
+        dfs(depth + 1, i + 1)
+        arr.pop()
+
+def main():
+    global N, M, arr
+    N, M = map(int, input().split())
+    arr = []
+    dfs(0, 1)
+    
+if __name__ == "__main__":
+    main()
