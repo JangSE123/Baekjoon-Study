8cfdce68346d6941cfc5605d543c52f7a65d7db1
JangSE123 <jse010206@gmail.com>
2025-12-16 16:48:21 +0900

    solve: Baekjoon Stage21
    🎯 Stage: 21 - Dynamic Programming 1
    📝 Problems Solved: 9461
    🛠️ Languages Used: Java

---
+++ Stage21/Java/Problem9461.java
@@ -0,0 +1,29 @@
+package Stage21.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem9461 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[101];
+        dp[1] = dp[2] = dp[3] = 1;
+        dp[4] = dp[5] = 2;
+
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
