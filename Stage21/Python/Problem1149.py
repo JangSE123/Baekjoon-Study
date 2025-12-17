131a932a7f65dc2f8cc6bd7baa823278ad7c6fcd
JangSE123 <jse010206@gmail.com>
2025-12-17 15:34:59 +0900

    solve: Baekjoon Stage21
    🎯 Stage: 21 - Dynamic Programming 1
    📝 Problems Solved: 1149
    🛠️ Languages Used: Python

---
+++ Stage21/Python/Problem1149.py
@@ -0,0 +1,13 @@
+def main():
+    n = int(input())
+    dp = [list(map(int, input().split())) for _ in range(n)]
+
+    for i in range(1, n):
+        dp[i][0] += min(dp[i - 1][1], dp[i - 1][2])
+        dp[i][1] += min(dp[i - 1][0], dp[i - 1][2])
+        dp[i][2] += min(dp[i - 1][0], dp[i - 1][1])
+        
+    print(min(dp[n - 1]))
+    
+if __name__ == "__main__":
+    main()
