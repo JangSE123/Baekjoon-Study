10aee1860f78bb8059e7b2230368ffcf8605ce25
JangSE123 <jse010206@gmail.com>
2025-12-23 18:51:15 +0900

    solve: Baekjoon Stage21
    🎯 Stage: 21 - Dynamic Programming 1
    📝 Problems Solved: 11053
    🛠️ Languages Used: Java

---
+++ Stage21/Java/Problem11053.java
@@ -0,0 +1,38 @@
+package Stage21.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(ans);
+        br.close();
+    }
+}
