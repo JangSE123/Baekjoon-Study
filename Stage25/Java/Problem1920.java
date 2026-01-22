d654660305f6574fc01f4bffcc454321ca8c078a
JangSE123 <jse010206@gmail.com>
2026-01-22 16:17:12 +0900

    solve: Baekjoon Stage25
    🎯 Stage: 25 - Binary Search
    📝 Problems Solved: 1920
    🛠️ Languages Used: Java

---
+++ Stage25/Java/Problem1920.java
@@ -0,0 +1,51 @@
+package Stage25.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem1920 {
+    static int[] arr;
+
+    static boolean binarySearch(int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) return true;
+            else if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(binarySearch(target) ? 1 : 0).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
