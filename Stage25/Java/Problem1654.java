04a7e807a690a9f39651693e8be3736581793b3b
JangSE123 <jse010206@gmail.com>
2026-01-27 12:57:07 +0900

    solve: Baekjoon Stage25
    🎯 Stage: 25 - Binary Search
    📝 Problems Solved: 1654
    🛠️ Languages Used: Java

---
+++ Stage25/Java/Problem1654.java
@@ -0,0 +1,47 @@
+package Stage25.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem1654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[K];
+        long max = 0;
+
+        for (int i = 0; i < K; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        long left = 1;
+        long right = max;
+        long answer = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            long count = 0;
+            for (long x : arr) {
+                count += x / mid;
+            }
+
+            if (count >= N) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+}
