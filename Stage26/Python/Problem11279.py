10ca7d06a3893acc5f1ec7a4bf6f6083222b8a2a
JangSE123 <jse010206@gmail.com>
2026-02-04 16:36:05 +0900

    solve: Baekjoon Stage26
    🎯 Stage: 26 - Priority Queue
    📝 Problems Solved: 11279
    🛠️ Languages Used: Python

---
+++ Stage26/Python/Problem11279.py
@@ -0,0 +1,22 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+def main():
+    N = int(input())
+    heap = []
+    
+    for _ in range(N):
+        x = int(input())
+        
+        if x == 0:
+            if not heap:
+                print(0)
+            else:
+                print(-heapq.heappop(heap))
+        else:
+            heapq.heappush(heap, -x)
+
+if __name__ == "__main__":
+    main()
