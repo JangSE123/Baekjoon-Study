ba8a477fc3ecc0b1ef3d11060529cf4b4359973e
JangSE123 <jse010206@gmail.com>
2026-02-05 17:24:34 +0900

    solve: Baekjoon Stage26
    🎯 Stage: 26 - Priority Queue
    📝 Problems Solved: 2075
    🛠️ Languages Used: Java

---
+++ Stage26/Java/Problem2075.java
@@ -0,0 +1,31 @@
+package Stage26.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Problem2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int x = Integer.parseInt(st.nextToken());
+                pq.offer(x);
+
+                if (pq.size() > N) {
+                    pq.poll();
+                }
+            }
+        }
+
+        System.out.println(pq.peek());
+        br.close();
+    }
+}
