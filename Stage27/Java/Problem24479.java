89b8c7db3644781138e697f96558bc2000a10b59
JangSE123 <jse010206@gmail.com>
2026-02-09 16:26:15 +0900

    solve: Baekjoon Stage27
    🎯 Stage: 27 - Graph and Traversal
    📝 Problems Solved: 24479
    🛠️ Languages Used: Java

---
+++ Stage27/Java/Problem24479.java
@@ -0,0 +1,68 @@
+package Stage27.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Problem24479 {
+    static int N, M, R;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int[] order;
+    static int cnt = 1;
+
+    static void dfs(int cur) {
+        visited[cur] = true;
+        order[cur] = cnt++;
+
+        for (int next : graph[cur]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        visited = new boolean[N + 1];
+        order = new int[N + 1];
+
+        dfs(R);
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(order[i]).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
