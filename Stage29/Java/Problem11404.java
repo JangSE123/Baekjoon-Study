d930ad0d363927729023baa576bf587d0c4c0d31
JangSE123 <jse010206@gmail.com>
2026-03-18 16:02:27 +0900

    solve: Baekjoon Stage29
    🎯 Stage: 29 - Shortest Path
    📝 Problems Solved: 11404
    🛠️ Languages Used: Java

---
+++ Stage29/Java/Problem11404.java
@@ -0,0 +1,61 @@
+package Stage29.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem11404 {
+    static final int INF = 1000000000;
+    static int n, m;
+    static int[][] dist;
+
+    static void floyd() {
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    if (dist[j][k] > dist[j][i] + dist[i][k]) {
+                        dist[j][k] = dist[j][i] + dist[i][k];
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        dist = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+
+        floyd();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
