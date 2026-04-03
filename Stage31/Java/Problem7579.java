b2109b9c01cc90a043e8b6672b40fdc233443062
JangSE123 <jse010206@gmail.com>
2026-04-03 18:12:31 +0900

    solve: Baekjoon Stage31
    🎯 Stage: 31 - Dynamic Programming 2
    📝 Problems Solved: 7579
    🛠️ Languages Used: Java

---
+++ Stage31/Java/Problem7579.java
@@ -0,0 +1,47 @@
+package Stage31.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem7579 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] memory = new int[N];
+        int[] cost = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            memory[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int maxCost = 0;
+        for (int i = 0; i < N; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+            maxCost += cost[i];
+        }
+
+        int[] dp = new int[maxCost + 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int c = maxCost; c >= cost[i]; c--) {
+                dp[c] = Math.max(dp[c], dp[c - cost[i]] + memory[i]);
+            }
+        }
+
+        for (int c = 0; c <= maxCost; c++) {
+            if (dp[c] >= M) {
+                System.out.println(c);
+                break;
+            }
+        }
+    }
+}
